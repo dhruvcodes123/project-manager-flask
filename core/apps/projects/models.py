5b6584b11887a634af02fc103343067d67cdccdb
dhruvcodes123 <dhruvpatel167413@gmail.com>
2023-11-03 21:53:05 +0530

    feat: Set up the SQL database and 'projects' table.

--- core/apps/projects/models.py
+++ core/apps/projects/models.py
@@ -1,2 +1,10 @@
-# models.py
-# The database models for this Flask application will be defined here.
+from core import db
+from datetime import datetime
+
+
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
